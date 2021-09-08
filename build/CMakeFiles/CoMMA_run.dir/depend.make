e942b3dab9efeda6ed8dc812393e59dc2b8ffd2f
albiremo <albertoremigi92@gmail.com>
2021-09-08 19:09:39 +0200

    cleaned folder

--- build/CMakeFiles/CoMMA_run.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CoMMA_run.
-# This may be replaced when dependencies are built.
--- build/Google_tests/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /stck/aremigi/CoMMA/Google_tests/lib/googlemock
-# Build directory: /stck/aremigi/CoMMA/build/Google_tests/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
