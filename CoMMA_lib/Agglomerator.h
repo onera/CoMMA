b6f00b417b9e05ba549149fe56d82e30320131b6
albiremo <albertoremigi92@gmail.com>
2021-11-23 16:58:45 +0100

    added comments doxygen style

--- CoMMA_lib/Agglomerator.h
+++ CoMMA_lib/Agglomerator.h
@@ -11,34 +11,47 @@
 #include "First_Order_Neighbourhood.h"
 #include "Triconnected_graph.h"
 #include "Util_graph.h"
-    //"""
-    // Main Class of the CoMMA library, containing the methods to operate the agglomeration
-    // process. It is constructed and the methods are prevalently called from the function agglomerate_one_level in
-    // CoMMA.cpp. The constructor require the following parameters:
-    //:param Dual_Graph: the struct Dual Graph 
-    //:param verbose: [boolean]
-    //:param is_visu_data_stored: parameter to store visualization data
-    //:param dimension: dimension of the problem
-    //"""
+
+
 // forward definition to keep the agglomerators in one file
+
+
 class Agglomerator_Anisotropic;
 class Agglomerator_Isotropic;
 
+/** @brief A class responsible to do the interface between the different kinds of
+ * agglomerator
+ *  @author Alberto Remigi and Nicolas Lantos
+ */
 class Agglomerator {
    public:
-   // Constructor 
+   /** @brief The constructor of the interface
+    *  @param[in] graph    *Dual Graph* object that determines the connectivity of the matrix 
+    *  @param[in] verbose  a boolean to determine if to store the visualization data
+    *  @param[in] dimension the dimension of the problem*/
    Agglomerator(Dual_Graph &graph,
                  unsigned short int verbose = 0,
                  bool is_visu_data_stored = false,
                  int dimension = 3);
-    // Destructor
+    /** @brief The destructor of the class */ 
     ~Agglomerator() {
         if (_cc_graph != NULL) {
             delete _cc_graph->_fc_graph.seeds_pool;
             delete _cc_graph;
         }
     };
-    // Set the agglomerator parameters
+    /** @brief Pure virtual function where the implementation is specified in the 
+     * child classes. The task of the pure virtual function is to set the parameters of
+     * dermine the cardinality limits with respect to the parameters passed
+     * @param[in] kind_of_agglomeration Parameter that determine the type of agglomeration algorithm used
+     * for the *Isotropic* agglomeration algorithm. The choices are between: - basic; -triconnected. Set
+     * as default to basic.
+     * @param[in] goal_card goal cardinality of the coarse cell (set as default to -1 indicating in our case
+     * the maximum value)
+     * @param[in] min_card minimum cardinality of the coarse cell(set as default to -1 indicating in our case
+     * the maximum value)
+     * @param[in] max_card maximum cardinality of the coarse cell(set as default to -1 indicating in our case
+     * the maximum value)*/    
     virtual void set_agglomeration_parameter(
             string kind_of_agglomerator = "basic",
             short goal_card = -1,
@@ -47,54 +60,55 @@ class Agglomerator {
 
 
     protected :
-    // Protected member variables, accessible to the derived
-    // classes
-    // dimensionality of the problem (_dimension = 2 -> 2D, _dimension = 3 -> 3D)
+    
+    /** @brief dimensionality of the problem (_dimension = 2 -> 2D, _dimension = 3 -> 3D)*/
     int _dimension;    
-    // boolean to define if it is anisotropic or not TODO: check if we can get rid of it
+    /** @brief boolean to define if it is anisotropic or not. It is set as default to false. 
+     *  @todo: check if we can get rid of _is_anisotropic variable*/
     bool _is_anisotropic = false;
-    // string to define the kindo of algorithm used for the agglomerator (basic or triconnected)
+    /** @brief string to define the kindo of algorithm used for the agglomerator (basic or triconnected)*/
     string _kind_of_agglomerator;
-    // minimum number of neighborhood
+    /** @brief minimum number of neighborhood. Set as default to 3.*/
     short _min_neighbourhood = 3;
-    // max number of neighborhood
+    /** @brief max number of neighborhood. Set as default to 6.*/
     short int _max_neighbourhood = 6;
-    // minimum number of neighborhood for the correction step
+    /** @brief minimum number of neighborhood for the correction step. Set as default to 1.*/
     short _min_neighbourhood_correction_step = 1;
-    // maximum number of neighborhood for the correction
+    /** @brief maximum number of neighborhood for the correction. Set as default to 3.*/ 
     short _max_neighbourhood_correction_step = 3;
-    // minimum cardinality
+    /** @brief minimum cardinality. Set as default to -1 (meaning the maximum possible).*/
     short _min_card = -1;
-    // maximum cardinality
+    /** @brief maximum cardinality. Set as default to -1 (meaning the maximum possible)*/
     short _max_card = -1;
-    // goal cardinality
+    /** @brief Goal cardinality. Set as default to -1 (meaning the maximum possible)*/
     short _goal_card = -1;  
-    // Threshold cardinality
+    /** @brief Threshold cardinality. Set as default to -1 (meaning the maximum possible)*/
     short _threshold_card = -1;
-    // list of number of cells
+    /** @brief List of number of cells per coarse cell created.*/
     vector<long> _l_nb_of_cells;
-    // Fine cells graph (constructed from fc_graph)
+    /** @brief Dual_Graph object detemining Fine cells graph and hence the connectivity.*/
     Dual_Graph _fc_graph;
-    // Coarse Cell Graph pointer
+    /** @brief pointer to Coarse Cell Graph element */
     Coarse_Cell_Graph *_cc_graph; 
-    // Visualization section
-    // flag to determine if visualization data are stored
+    /** @brief boolean flag to determine if visualization data are stored*/
     bool _is_visu_data_stored;
-    // flag for the verbose mode
+    /** @brief integer flag for the verbose mode.*/
     int _verbose;
 };
 
+/** @brief Agglomerator_Anisotropic class is a child class of the Agglomerator class
+ * that specializes the implementation to the case of Anisotropic agglomeration.*/
 class Agglomerator_Anisotropic : public Agglomerator{
    // Constructor 
    public:
+   /** @brief Constructor. The constructor takes as arguments the same arguments of the father and
+    * in this way activates also the constructor of the base class.*/
    Agglomerator_Anisotropic(Dual_Graph &graph,unsigned short int verbose = 0,bool is_visu_data_stored = false,int dimension = 3);
-    // Destructor
+   /** @brief Destructor*/
     ~Agglomerator_Anisotropic();
-    //    clean_agglomeration_lines(_v_lines[0]);
-    //    clean_agglomeration_lines(_v_lines[1]);
-    // Specialization of the pure virtual method in the base class to fill
-    // the parameter space
-
+   /** @brief Specialization of the pure virtual function to the class Agglomerator_Anisotropic. 
+    * We add the override key as a guard to possible mistakes:
+    * https://stackoverflow.com/questions/46446652/is-there-any-point-in-using-override-when-overriding-a-pure-virtual-function*/
     void set_agglomeration_parameter(
          string kind_of_agglomerator = "basic",
          short goal_card = -1,
@@ -102,29 +116,34 @@ class Agglomerator_Anisotropic : public Agglomerator{
          short max_card = -1) override;
 
     protected:
-    // Vector of set of the anisotropic compliant of fine cells
+    /** @brief Vector of set of the anisotropic compliant of fine cells*/
     vector<unordered_set<long>> _v_of_s_anisotropic_compliant_fc;
-    // Vector of number of Anisotropic agglomeration lines
+    /** @brief Vector of number of Anisotropic agglomeration lines*/
     vector<unsigned long> _v_nb_lines;
-    // _v_lines : Agglomeration lines structure:
-    // vector : level
-    // forward list : identifier of the line
-    // deque : line cells
-    // e.g _v_lines[0] --> agglomeration lines at the finest level
+    /** @brief _v_lines : Agglomeration lines structure:
+    * vector : level
+    * forward list : identifier of the line
+    * deque : line cells
+    * e.g _v_lines[0] --> agglomeration lines at the finest level*/
     vector<forward_list<deque<long> *>> _v_lines;  
 };
 
+/** @brief Agglomerator_Isotropic class is a child class of the Agglomerator class
+ * that specializes the implementation to the case of Isotropic agglomeration.*/
 class Agglomerator_Isotropic : Agglomerator{
-   // Constructor 
    public:
-   Agglomerator_Isotropic (Dual_Graph &graph,
+    /** @brief Constructor. The constructor takes as arguments the same arguments of the father and
+    * in this way activates also the constructor of the base class.*/
+    Agglomerator_Isotropic (Dual_Graph &graph,
                  unsigned short int verbose = 0,
                  bool is_visu_data_stored = false,
                  int dimension = 3); 
-    // Destructor
+    /** @brief Destructor*/
     ~Agglomerator_Isotropic();
-
-   void set_agglomeration_parameter(
+   /** @brief Specialization of the pure virtual function to the class Agglomerator_Anisotropic. 
+    * We add the override key as a guard to possible mistakes:
+    * https://stackoverflow.com/questions/46446652/is-there-any-point-in-using-override-when-overriding-a-pure-virtual-function*/
+    void set_agglomeration_parameter(
          string kind_of_agglomerator = "basic",
          short goal_card = -1,
          short min_card = -1,
