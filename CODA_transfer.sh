72c4adf844592d1a9a8f1d9a1c78c1045f91736d
albiremo <Alberto.Remigi@onera.fr>
2022-03-12 08:31:32 +0100

    removed b

--- CODA_transfer.sh
+++ CODA_transfer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CODA_INSTALL="../multigrid/algorithm_comma_brench/thirdparty/comma/src/"
-CODA_HEADER="../multigrid/algorithm_comma_brench/thirdparty/comma/include/Comma/"
+CODA_INSTALL="../../multigrid/algorithm_comma_brench/thirdparty/comma/src/"
+CODA_HEADER="../../multigrid/algorithm_comma_brench/thirdparty/comma/include/Comma/"
 
 echo "Removing previous version"
 rm -r -v ${CODA_INSTALL}*
