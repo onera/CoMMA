224872ee528cc1ccd07d2ec57a2a49a1a8195fe4
albiremo <Alberto.Remigi@onera.fr>
2022-05-18 09:54:50 +0200

    cleaned util

--- CODA_transfer.sh
+++ CODA_transfer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CODA_INSTALL="../../multigrid/negev_new_comma/thirdparty/comma/src/"
-CODA_HEADER="../../multigrid/negev_new_comma/thirdparty/comma/include/Comma/"
+CODA_INSTALL="../../multigrid_valid/negev/thirdparty/comma/src/"
+CODA_HEADER="../../multigrid_valid/negev/thirdparty/comma/include/Comma/"
 
 echo "Removing previous version"
 rm -r -v ${CODA_INSTALL}*
--- CoMMA_lib/Util.h
+++ CoMMA_lib/Util.h
@@ -6,44 +6,11 @@
 #define COMMA_PROJECT_UTIL_H
 
 #include <assert.h>
-#include <deque>
-#include <queue>
-#include <vector>
 #include <unordered_set>
 #include <unordered_map>
-#include <list>
-#include <forward_list>
-#include <cmath>
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <algorithm>
-#include "Dual_Graph.h"
 
 using namespace std;
 
-void convert_agglomeration_lines_arrays_to_agglomeration_lines(
-        const unsigned long fineAgglomerationLines_array_Idx_size,
-        const unsigned long fineAgglomerationLines_array_size,
-        vector<long> &fineAgglomerationLines_array_Idx,
-        vector<long> &fineAgglomerationLines_array,
-        long &nb_agglomeration_lines,
-        forward_list<deque<long> *> &agglomeration_lines);
-
-forward_list<deque < long> *> convertFineAgglomerationLines_Arrays_to_agglomeration_lines(
-        unsigned long fineAgglomerationLines_array_Idx_size,
-        unsigned long fineAgglomerationLines_array_size,
-        long *fineAgglomerationLines_array_Idx,
-        long *fineAgglomerationLines_array);
-
-forward_list<deque < long> > convertFineAgglomerationLines_arraysToFineAgglomerationLines_without_pointers(
-        unsigned long fineAgglomerationLines_array_Idx_size,
-        unsigned long fineAgglomerationLines_array_size,
-        long *fineAgglomerationLines_array_Idx,
-        long *fineAgglomerationLines_array);
-forward_list<int> test();
-
-list<unordered_set<long>> parts_list(vector<long> seq, int length);
 
 inline unordered_set<long> d_keys_to_set(const unordered_map<long,short> &dict) {
     unordered_set<long> s_neighbours_of_seed = {};
@@ -52,92 +19,5 @@ inline unordered_set<long> d_keys_to_set(const unordered_map<long,short> &dict) 
     }
     return s_neighbours_of_seed;
 }
-
-void print_queue(queue<long> q);
-
-void print_agglomeration_lines(forward_list<deque<long> *> lines);
-
-forward_list<deque<long> *> copy_agglomeration_lines(forward_list<deque<long> *>);
-
-void clean_agglomeration_lines(forward_list<deque<long> *>);
-
-bool compare_maps_keys(const unordered_map<long, unsigned short> &m_1, const unordered_map<long, unsigned short> &m_2);
-
-list<unordered_set<long>> partsList(vector<long> seq, int length = 0);
-
-bool checkConnectivity_w_set(unordered_set<long> listFineCells, long *matrixAdj_CRS_row_ptr, long *matrixAdj_CRS_col_ind, int verbose = 0);
-
-int computeNumberOfCommonFaces(long iFine, long iCoarse,
-                               long *matrixAdj_CRS_row_ptr,
-                               long *matrixAdj_CRS_col_ind,
-                               long *fine_Cell_indices_To_Coarse_Cell_Indices);
-
-void store_agglomeration_datas(long (&sizes)[10],
-        // Dual graph:
-                           const vector<long> &adjMatrix_row_ptr,
-                           const vector<long> &adjMatrix_col_ind,
-                           const vector<double> &adjMatrix_areaValues,
-                           const vector<double> &volumes,
-
-
-                           vector<long> &arrayOfFineAnisotropicCompliantCells,
-
-                           const vector<long> &isOnFineBnd,
-                           vector<long> &array_is_on_valley,
-                           vector<long> &array_is_on_ridge,
-                           vector<long> &array_is_on_corner,
-
-        // Agglomeration argument
-                           long isFirstAgglomeration_long,
-
-                           long is_anisotropic_long,
-
-
-                           vector<long> fineCellToCoarseCell,
-                       
-                           vector<long> &agglomerationLines_Idx,  // In & out
-                           vector<long> &agglomerationLines,  // In & out
-
-
-
-                               long dimension,
-                               long goalCard,
-                               long minCard,
-                               long maxCard,
-                               long checks_long,
-                               long verbose_long); 
-
-
-void read_agglomeration_datas_from_file(std::string filename,
-                                        long *sizes,
-                                        long *&adjMatrix_row_ptr,
-                                        long *&adjMatrix_col_ind,
-                                        double *&adjMatrix_areaValues,
-                                        double *&volumes,
-
-                                        long *&arrayOfFineAnisotropicCompliantCells,
-
-                                        long *&isOnFineBnd_l,
-                                        long *&array_isOnValley,
-                                        long *&array_isOnRidge,
-                                        long *&array_isOnCorner,
-                                        long *isFirstAgglomeration_long,
-                                        long *isAnisotropic_long,
-
-                                        long *&agglomerationLines_Idx,
-                                        long *&agglomerationLines,
-
-                                        long *dimension,
-                                        long *goalCard,
-                                        long *minCard,
-                                        long *maxCard
-);
-
-long read_long_array(std::fstream &stream, long *&array);
-
-long read_double_array(std::fstream &stream,
-                       double *&array);
-
-long read_long(std::fstream &stream);
 
 #endif //COMMA_PROJECT_UTIL_H
