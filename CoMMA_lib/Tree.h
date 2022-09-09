ad10cc784ea1aabe9d910a020cac61549492c2c3
Riccardo Milani <ricc.milani@gmail.com>
2022-09-09 09:19:55 +0200

    chore: Add headers as per #20

--- CoMMA_lib/Tree.h
+++ CoMMA_lib/Tree.h
@@ -1,6 +1,28 @@
 #ifndef COMMA_PROJECT_TREE_H
 #define COMMA_PROJECT_TREE_H
 
+/*
+ *  * CoMMA
+    * Copyright © 2022 ONERA
+    *
+    * Authors and Architects : Alberto Remigi and Nicolas Lantos
+    *
+    * Contributors : Riccardo Milani
+    *
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    *
+    * You should have received a copy of the GNU General Public License
+    * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <vector>
 #include <cstddef>
 #include <memory>
--- CoMMA_lib/templateHelpers.h
+++ CoMMA_lib/templateHelpers.h
@@ -1,6 +1,28 @@
 #ifndef COMMA_PROJECT_TEMPLATE_HELPERS_H
 #define COMMA_PROJECT_TEMPLATE_HELPERS_H
 
+/*
+ *  * CoMMA
+    * Copyright © 2022 ONERA
+    *
+    * Authors and Architects : Alberto Remigi and Nicolas Lantos
+    *
+    * Contributors : Riccardo Milani
+    *
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    *
+    * You should have received a copy of the GNU General Public License
+    * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 template <bool condition, typename typeA, typename typeB>
 void fill_value(typeA &A, typeB &B) {
   if
