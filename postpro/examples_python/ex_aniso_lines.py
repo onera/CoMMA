7b9faf94a9aa7fecc0328ceca72d356090e3c02a
Riccardo Milani <ricc.milani@gmail.com>
2022-11-19 11:13:54 +0100

    examples: Add example for drawing anistotropic lines

---
+++ postpro/examples_python/ex_aniso_lines.py
@@ -0,0 +1,142 @@
+"""This script create a small handmade 2D mesh with a block of anisotropic cells
+at the bottom, agglomerates and draw the mesh including the computed
+anisotropic lines using the related features of dualGPy.
+"""
+from CoMMA import *
+import meshio
+from dualGPy.Graph import Graph2D
+from dualGPy.Mesh import Mesh2D, Mesh3D
+import dualGPy.Utils as ut
+import numpy as np
+import meshio as mio
+
+isotropic_agglo_types = [
+        'Biconnected',
+        'Pure front advancing'
+        ]
+
+# USER PARAMETERS
+#################
+# Input-related parameters
+dimension = 2
+anisotropic = True
+## CoMMA parameters
+isFirstAgglomeration = True
+minCard, goalCard, maxCard = 4, 4, 4
+correction = False
+threshold_anisotropy = 1.5
+isotropic_agglo = 0 # 0 = Biconnected (standard), 1 = Pure front advancing
+
+# Output-related parameters
+# If < 1, the value associated to the coarse cells are the ID. Otherwise, only
+# numbers from 1 to the given value are used (it makes it easier to distinguish the
+# coarse cells in Paraview
+renumber_coarse = 10 # -1 #
+#########
+
+basename = "ani_3cell_test"
+outname = basename + '.vtu'
+
+print('Requested arguments:')
+print( ' [Input]')
+print(f' * {anisotropic=}')
+print(f' * {isFirstAgglomeration=}')
+print(f' * {minCard=}')
+print(f' * {goalCard=}')
+print(f' * {maxCard=}')
+print(f' * {correction=}')
+print(f' * {threshold_anisotropy=}')
+print(f' * isotropic_agglo={isotropic_agglo_types[isotropic_agglo]}')
+print( ' [Output]')
+renum = renumber_coarse > 1
+print(f' * Coarse cell renumbering={renum}' + (f" (from 0 to {renumber_coarse-1})" if renum else ""))
+print()
+
+ref_pts = np.zeros((7,2), dtype=float)
+ref_pts[:-1,1] = range(6)
+ref_pts[:-1,1] *= 0.2
+ref_pts[-1,1]  = 2
+
+points = np.empty((35,2), dtype=float)
+to_add = np.array([[1.,0.]], dtype=float)
+for i in range(5):
+    points[7*i:7*(i+1),:] = ref_pts + i*to_add
+
+# Cells are not ordered on purpose to try the scheme
+# Order of the points in the cell from bottom left node in anti-clockwork order
+base_cell = [0,7,8,1]
+cells = np.array([base_cell for _ in range(24)], dtype = int)
+# Bottom left nodes
+btlxnd = [0,1,2,3,4,5,12,11,10,9,8,7,16,17,15,18,14,19,26,25,21,24,22,23]
+# bottom left corner + base structure
+assert(len(btlxnd) == cells.shape[0])
+for i, nd in enumerate(btlxnd):
+    cells[i,:] += nd
+# print(cells)
+mio_m = mio.Mesh(points = points, cells = {'quad': cells})
+#mio_m.write('aniso_3cell.msh', file_format = "ansys")
+m = Mesh2D(mio_m)
+m.get_boundary_faces()
+m.ComputeGeometry()
+m.boundary_detection()
+
+g = Graph2D(m)
+g.get_CSR()
+
+adjMatrix_row_ptr= np.array(g.vertex , dtype='long')
+adjMatrix_col_ind= np.array(g.edges ,dtype='long')
+adjMatrix_areaValues = np.array(m.area,dtype='double')
+isOnBnd = np.array(m.boundary_cells,dtype='long')
+volumes = np.array(m.volume,dtype='double')
+nb_fc = len(g.vertex)-1
+fc_to_cc = np.full(nb_fc, -1, dtype = 'long')
+arrayOfFineAnisotropicCompliantCells = np.arange(nb_fc, dtype= 'long')
+agglomerationLines_Idx = np.array([0], dtype = 'long')
+agglomerationLines = np.array([0], dtype = 'long')
+
+print("CoMMA call")
+fc_to_cc_res,alines_Idx,alines = \
+        agglomerate_one_level(adjMatrix_row_ptr, adjMatrix_col_ind, adjMatrix_areaValues, volumes,
+                              arrayOfFineAnisotropicCompliantCells,isOnBnd, isFirstAgglomeration,
+                              anisotropic, threshold_anisotropy, isotropic_agglo,
+                              fc_to_cc,agglomerationLines_Idx,agglomerationLines,
+                              correction, dimension,goalCard,minCard,maxCard)
+
+print("Finalizing")
+# fine_cells_renum = ut.address_agglomerated_cells(fc_to_cc_res, renumber_coarse) if renum \
+        # else fc_to_res
+# As long as the data is composed of (integer) IDs, the following is equivalent but much faster
+fine_cells_renum = (np.asarray(fc_to_cc_res) % renumber_coarse) if renum \
+        else fc_to_cc_res
+
+agglo = []
+if len(m.mesh.cells) > 1:
+    # More than one element type -> Adapt cell_data to meshio cellblock format
+    start = 0
+    for cells_by_type in m.mesh.cells:
+        n_cell = cells_by_type.data.shape[0]
+        agglo.append( fine_cells_renum[start:start+n_cell] )
+        start += n_cell
+else:
+    agglo = [fine_cells_renum]
+
+dic = {}
+f2c = np.asarray(fc_to_cc_res, dtype=int)
+# For every line...
+for i in range(len(alines_Idx)-1):
+    mask = np.full(len(fc_to_cc_res), False)
+    # For every coarse cell in the line...
+    for cc in alines[alines_Idx[i]:alines_Idx[i+1]]:
+        # Take all the fine cells that are in the coarse cell
+        mask |= f2c == cc
+    dic[i] = np.flatnonzero(mask)
+line_draw = "test_lines.png"
+print(f"Drawing graph and lines in {line_draw}")
+m.draw_aggl_lines(line_draw, dic, -0.02, 2.02, -0.02, 2.02, show_axes = False)
+
+print(f"Writing in {outname}")
+meshio.Mesh(
+    m.mesh.points,
+    m.mesh.cells,
+    cell_data = {"agglomerate":agglo}
+).write(outname)
